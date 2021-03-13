da607862e9a4f588a549058b103b50e3ea5441cf
Caio Vincenzo <caio.vincenzo@estudante.ufjf.br>
2021-03-13 16:02:28 -0300

    Cria um mapa para uso na Cena

--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,6 @@
 import AssetManager from "./AssetsManager.js";
 import Cena from "./Cena.js";
+import Mapa from "./Mapa.js";
 import Mixer from "./Mixer.js";
 import Sprite from "./Sprite.js";
 
@@ -18,8 +19,13 @@ document.body.appendChild(assets.img("esqueleto"));
 document.body.appendChild(assets.img("orc"));
 
 const canvas = document.querySelector("canvas");
+canvas.width = 14*32;
+canvas.height = 10*32
 const ctx = canvas.getContext("2d");
 const cena1 = new Cena(canvas, assets);
+
+const mapa1 = new Mapa(10, 14, 32);
+cena1.configuraMapa(mapa1);
 
 const pc = new Sprite({ vx: 10 });
 const en1 = new Sprite({ x: 140, y: 100, w: 30, h: 30, color: "red" });
